f4bb70791f8913bf1dc696935e161083c6942cac
Robbie Bardijn <robbie.bardijn@gmail.com>
2022-05-15 21:09:34 +0200

    Add more global styling for the app and Typed global types comming back from the api

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -4,6 +4,7 @@ export const GlobalStyle = styled.createGlobalStyle`
   html {
     line-height: 1.15; /* 1 */
     -webkit-text-size-adjust: 100%; /* 2 */
+    height: 100%;
   }
 
   body {
@@ -13,6 +14,12 @@ export const GlobalStyle = styled.createGlobalStyle`
       "Helvetica Neue", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+  }
+
+  #root,
+  .App {
+    height: 100%;
   }
 
   code {
---
+++ src/global.d.ts
@@ -0,0 +1,40 @@
+export type Location = {
+  lat: number
+  long: number
+}
+
+export type City = {
+  coord: Location
+  country: string
+  id: number
+  name: string
+  population: number
+  sunrise: number
+  sunset: number
+  timezone: number
+}
+
+type MainInformation = {
+  feels_like: number
+  grnd_level: number
+  humidity: number
+  pressure: number
+  sea_level: number
+  temp: number
+  temp_kf: number
+  temp_max: number
+  temp_min: number
+}
+
+type WeatherForecast = {
+  description: string
+  icon: string
+  id: number
+  main: string
+}
+
+type Wind = {
+  deg: number
+  gust: number
+  speed: number
+}
